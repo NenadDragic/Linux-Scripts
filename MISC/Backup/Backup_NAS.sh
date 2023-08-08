efd140027273e22aa8cb27ffe40560e672bf097b
Nenad A. Dragic <nedad@Debian-Laptop.Dragic.com>
2023-08-08 14:09:20 +0200

    Folder changed

--- MISC/Backup/Backup_NAS.sh
+++ MISC/Backup/Backup_NAS.sh
@@ -6,11 +6,8 @@ if [ "$(whoami)" != "root" ]; then
     exit
 fi
 
-# Define the backup directory
-backup_dir="../../media/nenad/2F90E98B5A2F4030/Backup - Debian Laptop"
-
 # Backup the data
-sudo rsync -av -e "ssh -l Debian_Backup" 192.168.1.50::NetBackup/BackupData/Debian_Laptop/$(date +%Y-%m-%d)
+sudo rsync -av . -e "ssh -l Debian_Backup" 192.168.1.50::NetBackup/BackupData/Debian_Laptop/$(date +%Y-%m-%d)
 
 # Exit
 exit
