6d43f93321ae862aaaba8c54f18e0aa0b951050b
Nenad A. Dragic <nedad@Debian-Laptop.Dragic.com>
2023-10-03 10:31:12 +0200

    pic added

---
+++ MISC/Pic/Pic.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if libheif-examples package is installed
+if ! dpkg -l | grep -q libheif-examples; then
+    echo "libheif-examples package is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y libheif-examples
+fi
+
+# Create the folders
+mkdir -p ORG MOV MP4 CR2 HEIC
+
+# Convert HEIC files to JPEG and rename them
+for f in *.HEIC; do
+    heif-convert -q 100 "$f" "${f%.HEIC}.JPG"
+done
+
+# Move files to the corresponding folders
+mv -t MOV *.MOV
+mv -t MP4 *.MP4
+mv -t CR2 *.CR2
+mv -t HEIC *.HEIC
+
+
+# Copy JPEG files to ORG
+cp -t ORG *.JPG *.JPEG
+
+# Delete empty directories
+find . -type d -empty -delete
+
+
+echo All tasks completed.
