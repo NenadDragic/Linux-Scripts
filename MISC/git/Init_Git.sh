69a87d456410cdc01c2f3dd29c0f2c77237eb9c9
Nenad A. Dragic <nenad@Debian-Laptop.Dragic.com>
2023-11-08 19:43:38 +0100

    Added git init file

---
+++ MISC/git/Init_Git.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Clone all of NenadDragic's repositories
+# https://github.com/NenadDragic
+
+# Linux Scripts
+git clone git@github.com:NenadDragic/Linux-Scripts.git
+
+# Learning resources
+git clone git@github.com:NenadDragic/Learning.git
+
+# z-os
+git clone git@github.com:NenadDragic/z-os.git
+
+# bat
+git clone git@github.com:NenadDragic/bat.git
+
+# c++
+git clone git@github.com:NenadDragic/cpp.git
+
+# Bash scripts
+git clone git@github.com:NenadDragic/Bash.git
+
+# PowerShell scripts
+git clone git@github.com:NenadDragic/PowerShell.git
+
+# Cyber Security resources
+git clone git@github.com:NenadDragic/Cyber-Sec.git
+
+# Python scripts
+git clone git@github.com:NenadDragic/Python.git
+
+# C# scripts
+git clone git@github.com:NenadDragic/c-Sharp.git
