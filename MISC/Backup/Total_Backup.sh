8cfd1c99a6a807221c7fafdb6817ae1196c7d939
Nenad A. Dragic <nenad@Debian-Laptop.Dragic.com>
2023-12-29 14:28:50 +0100

    Changes to Total Backup

--- MISC/Backup/Total_Backup.sh
+++ MISC/Backup/Total_Backup.sh
@@ -8,7 +8,7 @@ if [ "$(whoami)" != "root" ]
 fi
 
 # Define the backup directory
-backup_dir="../../media/nedad/Dragic_Backup"
+backup_dir="../../media/nenad/Dragic_Backup"
 
 # Define the current date in the format YYYY-MM-DD
 date="$(date +%Y-%m-%d)"
@@ -17,18 +17,24 @@ date="$(date +%Y-%m-%d)"
 echo "Backing up drive 1..."
 drive_type=$(lsblk -no parttypename /dev/nvme0n1p1)
 pv -tpreb /dev/nvme0n1p1 | dd of="$backup_dir/$drive_type $date.img" bs=4M
-#dd if=/dev/nvme0n1p1 of="$backup_dir/$drive_type $date.img" bs=4M status=progress
+dd if=/dev/nvme0n1p1 of="$backup_dir/$drive_type $date.img" bs=4M status=progress
 
 # Backup drive 2
 echo "Backing up drive 2..."
 drive_type=$(lsblk -no parttypename /dev/nvme0n1p2)
 pv -tpreb /dev/nvme0n1p2 | dd of="$backup_dir/$drive_type $date.img" bs=4M
-#dd if=/dev/nvme0n1p2 of="$backup_dir/$drive_type $date.img" bs=4M status=progress
+dd if=/dev/nvme0n1p2 of="$backup_dir/$drive_type $date.img" bs=4M status=progress
 
 # Backup drive 3
 echo "Backing up drive 3..."
 drive_type=$(lsblk -no parttypename /dev/nvme0n1p3)
 pv -tpreb /dev/nvme0n1p3 | dd of="$backup_dir/$drive_type $date.img" bs=4M
-#dd if=/dev/nvme0n1p3 of="$backup_dir/$drive_type $date.img" bs=4M status=progress
+dd if=/dev/nvme0n1p3 of="$backup_dir/$drive_type $date.img" bs=4M status=progress
+
+echo "Backing up drive"
+drive_type=$(lsblk -no parttypename /dev/nvme0n1p3)
+pv -tpreb /dev/nvme0n1p3 | dd of="$backup_dir/$drive_type $date.img" bs=4M
+dd if=/dev/nvme0n1p3 of="$backup_dir/$drive_type $date.img" bs=4M status=progress
+
 
 echo "Backup complete!"
