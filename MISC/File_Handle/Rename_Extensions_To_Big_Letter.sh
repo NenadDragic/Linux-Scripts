a5b90d0d6cb070ef8e31e309daddcbe4e8e0af77
Nenad A. Dragic <nenad@Debian-Laptop.Dragic.com>
2024-01-18 22:33:05 +0100

    new files added

--- MISC/File_Handle/Rename_Extensions_To_Big_Letter.sh
+++ MISC/File_Handle/Rename_Extensions_To_Big_Letter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "Finding files in the current directory and its subdirectories whose names have lowercase file extensions and renaming them to have uppercase file extensions..."
+# Finding files in the current directory and its subdirectories whose names have lowercase file extensions and renaming them to have uppercase file extensions...
 
-find . -type f -iname '*.[a-z]*' -execdir rename -n 's/\.([a-z]+)/.\U$1/' {} \;
+#find . -type f -iname '*.[a-z]*' -execdir rename -n 's/\.([a-z]+)/.\U$1/' {} \;
+rename 'y/a-z/A-Z/' *
--- MISC/Pic/Pic.sh
+++ MISC/Pic/Pic.sh
@@ -10,6 +10,9 @@ fi
 # Create the folders
 mkdir -p ORG MOV MP4 CR2 HEIC
 
+# Big letters in extension
+rename 'y/a-z/A-Z/' *
+
 # Convert HEIC files to JPEG and rename them
 for f in *.HEIC; do
     heif-convert -q 100 "$f" "${f%.HEIC}.JPG"
