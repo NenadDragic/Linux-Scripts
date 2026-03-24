3f51db7a8ba714025fdd9edafcd21a24ab7a0873
Nenad Ahlstrøm Dragic <nenad@dragic.com>
2026-03-24 12:41:29 +0100

    Small changes made

--- MISC/git/Init_Git.sh
+++ MISC/git/Init_Git.sh
@@ -3,41 +3,44 @@
 # Clone all of NenadDragic's repositories
 # https://github.com/NenadDragic
 
-# Linux Scripts
-git clone git@github.com:NenadDragic/Linux-Scripts.git
-
-# Learning resources
-git clone git@github.com:NenadDragic/Learning.git
-
-# z-os
-git clone git@github.com:NenadDragic/z-os.git
-
-# z-os
-git clone git@github.com:NenadDragic/z-os_JB.git
+# Bash scripts
+git clone git@github.com:NenadDragic/Bash.git
 
 # bat
 git clone git@github.com:NenadDragic/bat.git
 
+# C# scripts
+git clone git@github.com:NenadDragic/c-Sharp.git
+
 # c++
 git clone git@github.com:NenadDragic/cpp.git
-
-# Bash scripts
-git clone git@github.com:NenadDragic/Bash.git
-
-# PowerShell scripts
-git clone git@github.com:NenadDragic/PowerShell.git
 
 # Cyber Security resources
 git clone git@github.com:NenadDragic/Cyber-Sec.git
 
-# Python scripts
-git clone git@github.com:NenadDragic/Python.git
-
-# C# scripts
-git clone git@github.com:NenadDragic/c-Sharp.git
-
 # JB Scripts
 git clone git@github.com:NenadDragic/JB-Scripts.git
 
+# Learning resources
+git clone git@github.com:NenadDragic/Learning.git
+
 # Linux Learning
 git clone git@github.com:NenadDragic/Linux_Learning.git
+
+# Linux Scripts
+git clone git@github.com:NenadDragic/Linux-Scripts.git
+
+# PowerShell scripts
+git clone git@github.com:NenadDragic/PowerShell.git
+
+# Python scripts
+git clone git@github.com:NenadDragic/Python.git
+
+# Website Source
+git clone git@github.com:NenadDragic/Web-Source.git
+
+# z-os
+git clone git@github.com:NenadDragic/z-os.git
+
+# z-os JB
+git clone git@github.com:NenadDragic/z-os_JB.git
